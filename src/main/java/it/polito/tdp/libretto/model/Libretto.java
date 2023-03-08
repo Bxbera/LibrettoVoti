8b7c5690797cc651fcec06f78f008460edbc489c
beraale <beraale@LAPTOP-EN6DPRNE>
2023-03-08 18:46:33 +0100

    Prima modifica

--- src/main/java/it/polito/tdp/libretto/model/Libretto.java
+++ src/main/java/it/polito/tdp/libretto/model/Libretto.java
@@ -1,5 +1,42 @@
 package it.polito.tdp.libretto.model;
 
-public class Libretto {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Libretto {
+	
+	private List<Voto> voti;
+	
+	public Libretto() {
+		this.voti = new ArrayList<Voto>();
+	}
+	
+	/**
+	 * Aggiungi un nuovo voto al libretto 
+	 * (per ora non fa nessun controllo)
+	 * @param v il Voto da aggiungere
+	 * @return true
+	 */
+	public boolean add(Voto v) {
+		return this.voti.add(v);
+	}
+	/*
+	public void add(String corso, int punti, LocalDate data) {}
+	*/
+	// tra i due metodi è meglio usare la prima, perchè un giorno potrei voler modificare
+	// la classe voto, quindi è molto meglio avere meno dipendenze possibili dai parametri
+	
+	public void stamp() {
+		for(Voto vi: this.voti) {
+			System.out.println(vi);
+		}
+	}
+	
+	public void stampaPuntiUguali(int valore) {
+		for(Voto vi: this.voti) {
+			if(vi.getPunti() == valore) {
+				System.out.println(vi);
+			}
+		}
+	}
 }
--- src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
@@ -1,10 +1,17 @@
 package it.polito.tdp.libretto.model;
 
+import java.time.LocalDate;
+
 public class TestLibretto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Libretto lib = new Libretto();
+		
+		lib.add(new Voto("Analisi 1", 29, LocalDate.of(2021,  2, 15)));
+		lib.add(new Voto("Fisica 1", 29, LocalDate.of(2022,  1, 20)));
+		//lib.stamp();
+		lib.stampaPuntiUguali(29);
 	}
 
 }
